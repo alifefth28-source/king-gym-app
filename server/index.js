3ebae8646178e52fadf50bd97e4682deb372918c
Radja <alifefth28@gmail.com>
2026-02-04 23:03:34 +0700

    FINAL_REPAIR_ALL_VARIABLES_AND_NODE_VERSION

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(cors({
 
 app.use(express.json());
 
-// Routes
+
 app.use('/api/bookings', bookingRoutes);
 app.use('/api/membership', membershipRoutes);
 app.use('/api/auth', authRoutes);
