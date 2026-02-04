a70c4a37ad9fd5f870d751f64bc736a9eafa437a
Radja <alifefth28@gmail.com>
2026-02-04 23:14:47 +0700

    REPAIR_CONTROLLERS

--- server/controllers/classController.js
+++ server/controllers/classController.js
@@ -1,6 +1,6 @@
 const Class = require('../models/Class');
 const Booking = require('../models/Booking'); 
-const sequelize = require('../config/db');
+const sequelize = require('../config/db'); //perubahan 
 
 
 exports.createClass = async (req, res) => {
