bf96f4d47c76ddba387e52142d57ac8c2df3419a
Radja <alifefth28@gmail.com>
2026-02-05 13:37:46 +0700

    REPAIR MODULE NOT FOUND

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,18 @@
 const { Sequelize } = require('sequelize');
 const mysql2 = require('mysql2'); 
-const pg = require('pg'); // Opsional, jaga-jaga jika Vercel bingung
 
-// 1. Ambil DATABASE_URL dari environment
+
+
 const connectionString = process.env.DATABASE_URL;
 
 if (!connectionString) {
     throw new Error('❌ DATABASE_URL tidak ditemukan di environment variables!');
 }
 
-// 2. Setup Sequelize dengan konfigurasi yang lebih aman
+// 2. Setup Sequelize
 const db = new Sequelize(connectionString, {
     dialect: 'mysql',
-    dialectModule: mysql2, // Wajib untuk Vercel
+    dialectModule: mysql2, 
     logging: false, 
     dialectOptions: {
         ssl: {
@@ -26,10 +26,8 @@ const db = new Sequelize(connectionString, {
         acquire: 30000,
         idle: 10000
     }
-}); // <--- TANDA TITIK KOMA INI SANGAT PENTING!
+});
 
-// 3. Cek koneksi
 console.log(`📡 Mencoba konek ke DB...`);
 
-// Export dulu baru authenticate (agar tidak memblokir import)
 module.exports = db;
